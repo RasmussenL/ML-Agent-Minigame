2743f5e0c500c2f7952fa83ed6316af04f1c06a6
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-10-04 12:38:49 -0600

    AddedMenuAndLoadingScreen
    for real

---
+++ ML-Agent-Project/Assets/Scripts/MenuStuff/Loading.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+        while (gameLevel.progress < 1)
+        {
+            Debug.Log("Loading...");
+            yield return new WaitForEndOfFrame();
+        }
+
+        
+    }
+}
