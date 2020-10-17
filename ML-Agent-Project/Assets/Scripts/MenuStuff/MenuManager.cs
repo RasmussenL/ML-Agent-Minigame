6c4ed5da7b55c65cc3ac1f8fc4fa35f0860837c4
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-10-17 17:44:23 -0600

    ImprovedUI
    Also adjusted track to just the floor for now until visual track is resised

--- ML-Agent-Project/Assets/Scripts/MenuStuff/MenuManager.cs
+++ ML-Agent-Project/Assets/Scripts/MenuStuff/MenuManager.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] panels;
+    public GameObject menuDef;
+    public GameObject controlsDef;
+
+    public void GoTo(string name)
     {
-        
+        foreach (GameObject p in panels)
+        {
+            if (p.name == name)
+            {
+                p.SetActive(true);
+                if(name == "Menu")
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                    EventSystem.current.SetSelectedGameObject(menuDef);
+                }
+                else
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                    EventSystem.current.SetSelectedGameObject(controlsDef);
+                }
+            }
+            else
+            {
+                p.SetActive(false);
+            }
+        }
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+         #if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+         #else
+         Application.Quit();
+         #endif
     }
 }
